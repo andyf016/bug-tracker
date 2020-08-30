9035025ea9f5559146b43787b28c670bc8068ff9
Andrew Fillenwarth <andyf016@gmail.com>
2020-08-30 01:37:47 -0400

    added links and custom field to admin page

--- myuser/admin.py
+++ myuser/admin.py
@@ -2,5 +2,5 @@ from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
 from myuser.models import CustomUser
 
-
+UserAdmin.fieldsets += ('Custom fields set', {'fields': ['bio']}),
 admin.site.register(CustomUser, UserAdmin)
